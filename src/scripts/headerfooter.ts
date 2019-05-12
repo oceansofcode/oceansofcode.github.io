397634df4aec8531847011ebdafba7f9024eb9ee
adamzis <adam.j.a@hotmail.com>
2019-05-12 11:11:16 -0400

    Header and footer now part of body

--- src/scripts/headerfooter.ts
+++ src/scripts/headerfooter.ts
@@ -3,16 +3,21 @@
  * This allows the header and footer to be changed only once per file despite this being a front-end only application.
  * TODO: hide the rest of the page until everything is loaded.
  */
-export default function() {
+export default () => {
     const header = document.createElement('header');
     const footer = document.createElement('footer');
+    const headerSrc = './src/html/header.html'
+    const footerSrc = './src/html/footer.html'
 
     // Showing ES6 fetch with arrow functions to include the header.
-    fetch('./src/html/header.html').then(response => response.text()).then(body => header.innerHTML = body).catch(err => console.log(err));
+    fetch(headerSrc)
+    .then(response => response.text())
+    .then(body => header.innerHTML = body)
+    .catch(err => console.log(err));
     
     // Using a tradational XHR to load the footer.
     let footerReq = new XMLHttpRequest();
-    footerReq.open('GET', './src/html/footer.html');
+    footerReq.open('GET', footerSrc);
     footerReq.onload = function() {
         if (footerReq.status >= 200 && footerReq.status < 400 && footerReq.readyState === 4) {
             footer.innerHTML = footerReq.responseText;
@@ -20,7 +25,7 @@ export default function() {
     }
     footerReq.send();
     
-    const body = document.querySelector('body');
-    body.parentElement.insertBefore(header, body);
-    body.parentElement.appendChild(footer);
+    const main = document.querySelector('#main');
+    main.insertBefore(header, main.firstChild);
+    main.appendChild(footer);
 }
