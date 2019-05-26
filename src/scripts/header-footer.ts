16497a2e0bb27f2f6a2f6a6dfd1d14a36e7cd842
adamzis <adam.j.a@hotmail.com>
2019-05-26 12:29:16 -0400

    TS Refactor and transition animations
    -Refactor how header footer added, they are now faster
    -Better separation of concerns
    -Added init function in main
    -Separated body transition ito its own function

--- src/scripts/header-footer.ts
+++ src/scripts/header-footer.ts
@@ -1,31 +1,32 @@
 /*
  * This module dynamically inserts the header and footer from their respective HTML files.
  * This allows the header and footer to be changed only once per file despite this being a front-end only application.
- * TODO: hide the rest of the page until everything is loaded and move footer out of main.
  */
-import themeToggle from './theme-toggle.js';
-export default async () => {
-    const body = document.querySelector('body');
-    const header = document.createElement('header');
-    const footer = document.createElement('footer');
-    const headerSrc = './src/html/header.html'
-    const footerSrc = './src/html/footer.html'
 
-    // Showing ES6 fetch with async to include the header.
-    let headerRes = await fetch(headerSrc);
-    let headerBody = await headerRes.text();
-    header.innerHTML = headerBody;
+/*
+ * Creates a header and footer element, and then asynchronously retrieves their data and inserts
+ * the data into them. Returns the header and footer as an object.
+ */
+export var getHeaderFooter = async () => {
+    const header: HTMLElement = document.createElement('header');
+    const footer: HTMLElement = document.createElement('footer');
+    const headerSrc: string = './src/html/header.html'
+    const footerSrc: string = './src/html/footer.html'
 
-    let footerRes = await fetch(footerSrc);
-    let footerBody = await footerRes.text();
-    footer.innerHTML = footerBody;
+    await Promise.all([
+        fetch(headerSrc).then(headerRes => headerRes.text()).then(headerText => header.innerHTML = headerText),
+        fetch(footerSrc).then(footerRes => footerRes.text()).then(footerText => footer.innerHTML = footerText),
+    ]);
     
-    const main = document.querySelector('#main');
+    return {header, footer};
+}
+
+/*
+ * Inserts a header and footer element inside the body before the main container.
+ */
+export function insertHeaderFooter(header: HTMLElement, footer: HTMLElement) {
+    const main: HTMLElement = document.querySelector('#main');
+
     main.parentNode.insertBefore(header, main);
     main.appendChild(footer);
-
-    // TODO Refactor this into it's own promise.
-    body.style.visibility = "visible";
-    body.style.opacity = "1";
-    themeToggle();
 }
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,7 +1,32 @@
 /*
  * Main module that is to be loaded at the beginning of every page.
  */
-import headerFooter from './header-footer.js';
+import { getHeaderFooter, insertHeaderFooter } from './header-footer.js';
+import themeToggle from './theme-toggle.js';
 {
-    headerFooter();
+    /*
+     * Initializes the page by retrieving the header and footer, inserting them, and then finally making the page visible.
+     */
+    (async function init() {
+        const headerFooter: {header: HTMLElement, footer: HTMLElement} = await getHeaderFooter();
+
+        insertHeaderFooter(headerFooter.header, headerFooter.footer);
+        themeToggle();
+
+        showBody();
+    })();
+
+    /*
+     * The body is initially not visible with an opacity of 0.
+     * This function sets how the transition will occur before
+     * making it occur
+     */
+    function showBody() {
+        const body: HTMLElement = document.querySelector('body');
+        const bodyFadeIn: string = "visibility 0s, opacity 1s linear"
+
+        body.style.transition = bodyFadeIn;
+        body.style.visibility = "visible";
+        body.style.opacity = "1";
+    }
 }
