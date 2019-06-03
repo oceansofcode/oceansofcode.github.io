202c603ae4a18659d846d3da3806dd577c8d1b68
adamzis <adam.j.a@hotmail.com>
2019-06-03 08:14:25 -0400

    Remove old angular placeholder

--- src/angular-stocks/src/app/app.component.spec.ts
+++ src/angular-stocks/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stocks'`, () => {
+  it(`should have as title 'angular-stocks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('stocks');
+    expect(app.title).toEqual('angular-stocks');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to stocks!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-stocks!');
   });
 });
