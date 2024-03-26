6eb968ec396e63c715f16443eeee44554005da2b
Adam <ad.kn1@icloud.com>
2024-03-26 11:35:48 -0400

    Create images.js

--- build-scripts/images.js
+++ build-scripts/images.js
@@ -1,3 +1,4 @@
+/* eslint-disable immutable/no-mutation */
 import { promises as fs, existsSync, mkdirSync } from 'fs';
 import sharp from 'sharp';
 
@@ -14,18 +15,63 @@ import sharp from 'sharp';
 });
 */
 
-const largeWidth = 1280;
-const smallWidth = 400;
+// Define the widths based on named sizing
+const width = {
+    small: 400,
+    large: 1280
+};
 
 const imgSrcDir = '../src/images';
 const imgDistDir = '../dist/images';
 
+// Ensure the main image dist directory and size subfolders exist
 if (!existsSync(imgDistDir)) {
     mkdirSync(imgDistDir);
 }
 
+for (const size in width) {
+    const sizeDistDir = `${imgDistDir}/${size}`;
+    if (!existsSync(sizeDistDir)) {
+        mkdirSync(sizeDistDir);
+    }
+}
+
 const imgs = (await fs.readdir(imgSrcDir, { withFileTypes: true, recursive: true }))
     .filter(dir => !dir.isDirectory())
-    .map(img => `${img.path}/${img.name}`);
+    .map(file => {
+        const name = file.name;
+        const path = file.path;
+        const fullPath = `${path}/${name}`;
 
-console.log(imgs);
+        return {
+            name,
+            path,
+            fullPath
+        };
+    });
+
+async function* imageFileGenerator(imgs) {
+    for (const img of imgs) {
+        console.log(`\nYielding ${img.name}`);
+
+        img.contents = await fs.readFile(img.fullPath);
+        yield img;
+    }
+}
+
+// Read each image one at a time from the generator and run sharp on them.
+for await (const image of imageFileGenerator(imgs)) {
+    for (const size in width) {
+        console.log(`Creating ${size} size for image ${image.name}`);
+
+        const imgSizeDistPath = `${imgDistDir}/${size}/${image.name}`.replace(/\.(png|jpg)$/, '.webp');
+        console.log(imgSizeDistPath);
+
+        const res = await sharp(image.fullPath)
+            .resize(width[size])
+            .webp()
+            .toFile(imgSizeDistPath);
+
+        console.log(res);
+    }
+}
